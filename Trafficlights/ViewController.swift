8c0c4c9b53be3c69935cfdfe2e7dc8ab9849619f
Ксения Гагина <kgagina@MacBook-Air.local>
2024-11-05 22:49:26 +0300

    Initial project

---
+++ Trafficlights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trafficlights
+//
+//  Created by Ксения Гагина on 05.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
