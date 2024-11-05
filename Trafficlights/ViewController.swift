b96ef4179baea42eed455d2d70eed8e92e620a47
Ксения Гагина <kgagina@MacBook-Air.local>
2024-11-05 23:42:02 +0300

    I have added UI elements

--- Trafficlights/ViewController.swift
+++ Trafficlights/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+  
+  // MARK: - Private propertes
+  
+  private let trafficLightHousingView = UIView()
+  private let verticalSupportView = UIView()
+  private let horizontalSupportView = UIView()
+  private let redCircleView = UIView()
+  private let yellowCircleView = UIView()
+  private let greenCircleView = UIView()
+  private let trafficLightStartButton = UIButton()
+  
+  // MARK: - Internal function
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .red
+    
+    
   }
-
-
 }
+// MARK: - Private functions
+
+private extension ViewController {
+  func settingLayout() {
+    [trafficLightHousingView, verticalSupportView, horizontalSupportView,
+     trafficLightStartButton].forEach {$0.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview($0)
+    }
+    [redCircleView, yellowCircleView, greenCircleView].forEach
+    {$0.translatesAutoresizingMaskIntoConstraints = false
+      trafficLightHousingView.addSubview($0)
+    }
+  }
+}
+
+
 
